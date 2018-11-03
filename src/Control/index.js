91f895848ff3b34132c3bf8d861e0cfa184e85c5
Marijn Ophorst <marijn@monomelodies.nl>
2018-11-03 15:48:06 +0100

    import attribution control

--- src/Control/index.js
+++ src/Control/index.js
@@ -3,7 +3,8 @@
 
 import Navigation from './Navigation';
 import Geolocate from './Geolocate';
+import Attribution from './Attribution';
 
-export default angular.module('idrisi.control', [Navigation, Geolocate])
+export default angular.module('idrisi.control', [Navigation, Geolocate, Attribution])
     .name;
 
