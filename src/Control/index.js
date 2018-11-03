97b7957862cfe42ffde02544c174e2a7fbbf9489
Marijn Ophorst <marijn@monomelodies.nl>
2018-11-03 15:52:27 +0100

    add fullscreen control

--- src/Control/index.js
+++ src/Control/index.js
@@ -5,7 +5,8 @@ import Navigation from './Navigation';
 import Geolocate from './Geolocate';
 import Attribution from './Attribution';
 import Scale from './Scale';
+import Fullscreen from './Fullscreen';
 
-export default angular.module('idrisi.control', [Navigation, Geolocate, Attribution, Scale])
+export default angular.module('idrisi.control', [Navigation, Geolocate, Attribution, Scale, Fullscreen])
     .name;
 
