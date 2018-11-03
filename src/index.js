90ae64f9c6b4de85ba5bfc858195ebbd801491b2
Marijn Ophorst <marijn@monomelodies.nl>
2018-11-03 16:29:43 +0100

    set access token from constant

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 
 "use strict";
 
+import mapboxgl from 'mapbox-gl';
 import Map from './Map';
 import Marker from './Marker';
 import Control from './Control';
 
 export default angular.module('idrisi', [Map, Marker, Control])
+    .run(['accessToken', accessToken => mapboxgl.accessToken = accessToken])
     .name;
 
