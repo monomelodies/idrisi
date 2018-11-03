defa2368a265f802a1136f5bdda42fa4a8195cc3
Marijn Ophorst <marijn@monomelodies.nl>
2018-11-03 11:15:03 +0100

    import marker

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import mapboxgl from 'mapbox-gl';
 window.mapboxgl = mapboxgl;
 
 import map from './Map';
+import marker from './Marker';
 
-export default angular.module('idrisi', [map])
+export default angular.module('idrisi', [map, marker])
     .name;
 
