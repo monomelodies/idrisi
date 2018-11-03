a5d13d22ccfd4a0949c9c5aa2fc1fa2166cbe015
Marijn Ophorst <marijn@monomelodies.nl>
2018-11-03 15:50:46 +0100

    add scale control

---
+++ src/Control/Scale.js
@@ -0,0 +1,32 @@
+
+"use strict";
+
+import mapboxgl from 'mapbox-gl';
+
+const wm = new WeakMap();
+
+class controller {
+
+    ['$onInit']() {
+        wm.set(this, new mapboxgl.ScaleControl({
+            maxWidth: this.maxWidth == undefined ? 100 : parseInt(this.maxWidth),
+            unit: this.unit || 'metric'
+        }));
+        this.parent.map.addControl(wm.get(this), this.location || 'top-right');
+    }
+};
+
+export default angular.module('idrisi.control.scale', [])
+    .component('idrisiScaleControl', {
+        controller,
+        require: {
+            parent: '^^idrisiMap'
+        },
+        bindings: {
+            maxWidth: '@',
+            unit: '@',
+            'location': '@'
+        }
+    })
+    .name;
+
