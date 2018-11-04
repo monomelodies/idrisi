6a9416006d52a690a846db409ed5ecdb24ea5b21
Marijn Ophorst <marijn@monomelodies.nl>
2018-11-04 09:09:09 +0100

    add ondestroy hook

--- src/Control/Scale.js
+++ src/Control/Scale.js
@@ -14,6 +14,11 @@ class controller {
         }));
         this.parent.map.addControl(wm.get(this), this.location || 'top-right');
     }
+
+    ['$onDestroy']() {
+        wm.delete(this);
+    }
+
 };
 
 export default angular.module('idrisi.control.scale', [])
