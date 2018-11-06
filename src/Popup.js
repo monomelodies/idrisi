887c6256c94b4c5b5d73fe1a9c55019db35dc78e
Marijn Ophorst <marijn@monomelodies.nl>
2018-11-06 08:37:21 +0100

    further attempts...

--- src/Popup.js
+++ src/Popup.js
@@ -5,45 +5,48 @@ import { Popup } from 'mapbox-gl';
 
 const elementWm = new WeakMap();
 const popupWm = new WeakMap();
+const domWm = new WeakMap();
 
 class controller {
 
     constructor($element) {
         elementWm.set(this, $element);
-        const options = {
-            closeButton: this.closeButton === undefined ? true : !!this.closeButton,
-            closeOnClick: this.closeOnClick == undefined ? true : !!this.closeOnClick
-        };
-        if (this.anchor) {
-            options.anchor = this.anchor;
-        }
-        if (this.offset) {
-            options.offset = this.offset;
-        }
-        if (this.className) {
-            options.className = this.className;
-        }
-        popupWm.set(this, new Popup(options));
     }
 
     set opened(newvalue) {
         if (newvalue) {
-            console.log('adding? :(');
-            popupWm.get(this).addTo(this.parent.parent.map);
-            console.log(popupWm.get(this));
-        } else {
+            const options = {
+                closeButton: this.closeButton === undefined ? true : !!this.closeButton,
+                closeOnClick: this.closeOnClick == undefined ? true : !!this.closeOnClick
+            };
+            if (this.anchor) {
+                options.anchor = this.anchor;
+            }
+            if (this.offset) {
+                options.offset = this.offset;
+            }
+            if (this.className) {
+                options.className = this.className;
+            }
+            popupWm.set(this, new Popup(options));
+            const popup = new Popup(options);
+            popup.setLngLat(this.parent.lngLat)
+                .setDOMContent(domWm.get(this));
+            popup.addTo(this.parent.parent.map);
+            console.log(popup);
+            popupWm.set(this, popup);
+        } else if (popupWm.get(this)) {
             popupWm.get(this).remove();
         }
     }
 
     get opened() {
-        return popupWm.get(this).isOpen();
+        const popup = popupWm.get(this);
+        return popup && popup.isOpen();
     }
 
     ['$postLink']() {
-        popupWm.get(this).setLngLat(this.parent.lngLat);
-        popupWm.get(this).setHTML(elementWm.get(this).html());
-        elementWm.get(this).css({display: 'none'});
+        domWm.set(this, elementWm.get(this)[0].firstChild);
     }
 
     ['$onDestroy']() {
