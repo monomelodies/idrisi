35ead9c3e1c25c365bd5a23b6c51eed8332f7044
Marijn Ophorst <marijn@monomelodies.nl>
2018-11-10 07:50:42 +0100

    prefix event name with on

--- src/Helpers/proxyEvents.js
+++ src/Helpers/proxyEvents.js
@@ -3,7 +3,7 @@
 
 export default function proxyEvents(key, object, events) {
     events.map(event => {
-        const name = event[0].toUpperCase() + event.substring(1);
+        const name = 'on' + event[0].toUpperCase() + event.substring(1);
         if (this[name]) {
             object.on(event, () => this[name]({key: object}));
         }
