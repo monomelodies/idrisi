03431ef7ecbe61289817a6963ec27384c3d270de
Marijn Ophorst <marijn@sensimedia.nl>
2018-11-20 14:44:17 +0100

    start setting up layers

---
+++ src/Layer/index.js
@@ -0,0 +1,28 @@
+
+"use strict";
+
+class controller {
+};
+
+export default angular.module('idrisi.layer', [])
+    .component('idrisiLayer', {
+        controller,
+        required: {
+            parent: '^^idrisiMap'
+        },
+        bindings: {
+            id: '@',
+            // fill, line, symbol, circle, heatmap, fill-extrusion, raster, hillshade, background
+            type: '@',
+            source: '@',
+            metadata: '<',
+            source: '@',
+            sourceLayer: '@',
+            minzoom: '@',
+            maxzoom: '@',
+            filter: '<',
+            layout: '<',
+            paint: '<'
+    })
+    .name;
+
