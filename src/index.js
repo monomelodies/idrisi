1d0bb775d5f8e7f71e942e624d6d443ae09fda62
Marijn Ophorst <marijn@monomelodies.nl>
2018-12-26 10:04:47 +0100

    place in global scope (but only if not exists)

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ import Marker from './Marker';
 import Control from './Control';
 import Popup from './Popup';
 
+if (!window.mapboxgl) {
+    window.mapboxgl = mapboxgl;
+}
+
 export default angular.module('idrisi', [Map, Layer, Marker, Control, Popup])
     .run(['accessToken', accessToken => mapboxgl.accessToken = accessToken])
     .name;
