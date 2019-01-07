515d0385c00453aad1ef1bde5b5964098e8b84e7
Marijn Ophorst <marijn@sensimedia.nl>
2019-01-07 15:33:48 +0100

    implement this with a listener (so we can react to changes)

--- src/Source.js
+++ src/Source.js
@@ -1,21 +1,34 @@
 
 "use strict";
 
+const wm = new WeakMap();
+
 class controller {
+
+    constructor($scope) {
+        wm.set(this, $scope);
+    }
 
     ['$postLink']() {
         this.parent.map.on('load', () => {
-            this.parent.map.addSource(this.id, {
-                type: this.type,
-                data: this.data,
-                cluster: this.cluster,
-                clusterMaxZoom: this.clusterMaxZoom,
-                clusterRadius: this.clusterRadius
+            wm.get(this).$watch('$ctrl.data', newval => {
+                if (this.parent.map.isSourceLoaded(this.id)) {
+                    this.parent.map.removeSource(this.id);
+                }
+                this.parent.map.addSource(this.id, {
+                    type: this.type,
+                    data: this.newval,
+                    cluster: this.cluster,
+                    clusterMaxZoom: this.clusterMaxZoom,
+                    clusterRadius: this.clusterRadius
+                });
             });
         });
     }
 
 };
+
+controller.$inject = ['$scope'];
 
 export default angular.module('idrisi.source', [])
     .component('idrisiSource', {
