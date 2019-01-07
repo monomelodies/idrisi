1c910a6bbe2760fdf516ef1dbbf87fb5182d2f7d
Marijn Ophorst <marijn@sensimedia.nl>
2019-01-07 14:52:41 +0100

    start setting up source initialization

--- src/Source.js
+++ src/Source.js
@@ -2,6 +2,19 @@
 "use strict";
 
 class controller {
+
+    ['$postLink']() {
+        this.parent.map.on('load', () => {
+            this.parent.map.addSource(this.id, {
+                type: this.type,
+                data: this.data,
+                cluster: this.cluster,
+                clusterMaxZoom: this.clusterMaxZoom,
+                clusterRadius: this.clusterRadius
+            });
+        });
+    }
+
 };
 
 export default angular.module('idrisi.source', [])
@@ -9,6 +22,15 @@ export default angular.module('idrisi.source', [])
         controller,
         require: {
             parent: '^^idrisiMap'
+        },
+        bindings: {
+            id: '@',
+            // vector, raster, raster-dem, geojson, image, video
+            type: '@',
+            data: '<',
+            cluster: '<',
+            clusterMaxZoom: '@',
+            clusterRadius: '@'
         }
     })
     .name;
