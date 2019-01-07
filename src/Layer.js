fe532b7b72ec39bff9bb95501efe915f37e030c7
Marijn Ophorst <marijn@sensimedia.nl>
2019-01-07 14:52:31 +0100

    start setting up of layer initialization

--- src/Layer.js
+++ src/Layer.js
@@ -2,12 +2,25 @@
 "use strict";
 
 class controller {
+
+    ['$postLink']() {
+        this.parent.parent.map.on('load', () => {
+            this.parent.parent.map.addLayer({
+                id: this.id,
+                type: this.type,
+                source: this.parent.id,
+                filter: this.filter || ['has', 'point_count'],
+                paint: this.paint
+            });
+        });
+    }
+
 };
 
 export default angular.module('idrisi.layer', [])
     .component('idrisiLayer', {
         controller,
-        required: {
+        require: {
             parent: '^^idrisiSource'
         },
         bindings: {
